eb60573991660eb6879b25abfc8202b16d20fece
Anik <ams9.anikmitra@gmail.com>
2023-04-06 02:37:20 +0530

    Factorial of  anumber

--- recursion/fact.c
+++ recursion/fact.c
@@ -11,5 +11,5 @@ int fact(int a) {
     if(a == 1) {    //base condition
         return 1;
     }
-    return (a*fact(a-1));
+    return (a*fact(a-1)); //calling itself again with decreeased value
 }
