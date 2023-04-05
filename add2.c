b60accd0b98d0d2e0239ddc3800a3b78c817d488
Anik <ams9.anikmitra@gmail.com>
2023-04-06 01:38:42 +0530

    Addition of 2 numbers using function

---
+++ add2.c
@@ -0,0 +1,12 @@
+/** Adding 2 numbers using a function*/
+#include<stdio.h>
+int add(int,int);
+void main() {
+    int a,b;
+    printf("Enter 2 numbers : \n");
+    scanf("%d%d",&a,&b);
+    printf("sum of %d and %d is : %d",a,b,add(a,b));
+}
+int add(int a,int b) {
+    return a+b;
+}
