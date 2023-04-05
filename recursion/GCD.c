e878b5ce870762dfe8d9de4a4381d9bc3c8f2b60
Anik <ams9.anikmitra@gmail.com>
2023-04-06 01:59:37 +0530

    GCD of 2 numbers using recursion

---
+++ recursion/GCD.c
@@ -0,0 +1,18 @@
+/** Finding GCD of 2 numbers using recuirison*/
+#include<stdio.h>
+int GCD(int,int);
+void main() {
+    int a,b;
+    printf("Enter 2 numbers\n");
+    scanf("%d%d",&a,&b);
+    printf("GCD of %d and %d is : %d",a,b,GCD(a,b));
+}
+int GCD(int x,int y) {
+    int c = x % y;
+    if(x > y && c == 0) {
+        return y;        
+    }
+    else {
+        return GCD(y,c);
+    }
+}
